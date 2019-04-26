381ae3b27ecb1a25e6e8269013cf6f330182a359
Eric Y. Theriault <etheriau@fxdevelopment.com>
2019-04-26 17:51:08 -0400

    Importing the V2 CreateAuthenticatedSessionToken from https://github.com/CHBase/chbase-java-sdk/blob/master/sdk/src/main/java/com/chbase/ApplicationAuthenticator.java which works both on HealthVault and on Lydia

--- sdk/src/main/java/com/microsoft/hsg/ApplicationAuthenticator.java
+++ sdk/src/main/java/com/microsoft/hsg/ApplicationAuthenticator.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2011 Microsoft Corp.
+ * (c) Copyright Microsoft Corporation. 
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ * This source is subject to the Microsoft Public License.
  *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL       
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,11 +17,16 @@ package com.microsoft.hsg;
 
 import java.security.PrivateKey;
 import java.security.Signature;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.codec.binary.Base64;
+import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
 
@@ -100,7 +105,8 @@ public class ApplicationAuthenticator implements Authenticator
 	{
 		this.appId = appId;
 	}
-	/**
+
+	/**
 	 * Gets the key store.
 	 * 
 	 * @return the key store
@@ -145,18 +151,22 @@ public class ApplicationAuthenticator implements Authenticator
 	
 		ensurePrivateKey();
 		
-		connection.setSharedSecret(null);
 		connection.setSessionToken(null);
-		sharedSecret = sharedSecretGenerator.generateSharedSecret();
-		Request castRequest = createAuthenticatedSessionTokenRequest(sharedSecret);
+		Request castRequest = createAuthenticatedSessionTokenRequest();
 		accessor.send(castRequest, connection);
 		Response response = accessor.getResponse();
 		
 		try
 		{
+			InputSource src = new InputSource(response.getInputStream());
+			DocumentBuilderFactory doc_factory =  DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = doc_factory.newDocumentBuilder();
+			Document doc = builder.parse(src);
 	        XPath xpath = XPathFactory.newInstance().newXPath();
 	        String exp = "//token";
-	        sessionToken = xpath.evaluate(exp, new InputSource(response.getInputStream()));
+	        sessionToken = xpath.evaluate(exp,doc);
+	        exp = "//shared-secret";
+	        sharedSecret=Base64.decodeBase64( xpath.evaluate(exp, doc).getBytes());	        
 		}
 		catch(Exception e)
 		{
@@ -173,33 +183,35 @@ public class ApplicationAuthenticator implements Authenticator
 	
     }
 
-	private Request createAuthenticatedSessionTokenRequest(
-			byte[] sharedSecret) 
+	private Request createAuthenticatedSessionTokenRequest() 
     {
 		try 
 		{
+			Date date = new Date();
+			String sign_date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S").format(date);
 	        StringBuilder contentBuilder = new StringBuilder();
 	        contentBuilder.append("<content><app-id>");
 	        contentBuilder.append(appId);
-	        contentBuilder.append("</app-id><shared-secret><hmac-alg algName=\"HMACSHA256\">");
-	        contentBuilder.append(new String(Base64.encodeBase64(sharedSecret)));
-	        contentBuilder.append("</hmac-alg></shared-secret></content>");
+	        contentBuilder.append("</app-id><hmac>HMACSHA256</hmac>");
+	        contentBuilder.append("<signing-time>"+ sign_date +"</signing-time>");
+	        contentBuilder.append("</content>");
 	        String content = contentBuilder.toString();
 	          
 	        StringBuilder infoBuilder = new StringBuilder();
 	        infoBuilder.append("<info><auth-info><app-id>");
 	        infoBuilder.append(appId);
-	        infoBuilder.append("</app-id><credential><appserver><sig digestMethod=\"SHA1\" sigMethod=\"RSA-SHA1\" thumbprint=\"");
+	        infoBuilder.append("</app-id><credential><appserver2><sig digestMethod=\"SHA1\" sigMethod=\"RSA-SHA1\" thumbprint=\"");
 	        infoBuilder.append(thumbprint);
 	        infoBuilder.append("\">");
 	        infoBuilder.append(new String(Base64.encodeBase64(signContent(content.getBytes("UTF-8")))));
 	        infoBuilder.append("</sig>");
 	        infoBuilder.append(content);
-	        infoBuilder.append("</appserver></credential></auth-info></info>");
+	        infoBuilder.append("</appserver2></credential></auth-info></info>");
 	        
 	        //TODO: create CASTRequest extends Request to include specific details
 	    	Request castRequest = new Request();
 	    	castRequest.setMethodName("CreateAuthenticatedSessionToken");
+	    	castRequest.setMethodVersion("2");
 	    	castRequest.setInfo(infoBuilder.toString());
 	    	
 	    	return castRequest;
